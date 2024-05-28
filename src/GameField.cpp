97311f12a89193ee2759ec8cb54d59c34ecb2105
Sitonc <morini.riccardo0100@gmail.com>
2024-05-28 10:55:31 +0900

    First Commit

---
+++ src/GameField.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Riccardo Morini on 2024/05/28.
+//
+
+#include "GameField.h"
---
+++ src/GameField.h
@@ -0,0 +1,18 @@
+//
+// Created by Riccardo Morini on 2024/05/28.
+//
+
+#ifndef GAMEFIELD_H
+#define GAMEFIELD_H
+
+
+
+class GameField {
+private:
+    const int m_widht = 800;
+    const int m_hight = 600;
+};
+
+
+
+#endif //GAMEFIELD_H
